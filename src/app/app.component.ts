e444a235cb0a92a5059c64e7a4c9343aa570dea9
Tate-T <tatjana.tarasovych@gmail.com>
2022-06-14 02:16:56 +0300

    styles of header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+// import { CurrencySaleService } from './_pages/currensy-sale/currency-sale.service';
+
+// interface FormSale {
+//   ccy: string;
+//   sale: number
+// }
 
 @Component({
   selector: 'app-root',
@@ -9,4 +15,22 @@ export class AppComponent {
   title = 'CURRENCY CONVERTER';
   
   titleForm = 'Select type of currency for SALE:'
+
+  // ratesList: FormSale [];
+
+  // constructor(private currencySaleService: CurrencySaleService) { }
+
+  // ngOnInit(): void {
+  //   this.currencySaleService.getCurrency().subscribe( res => {
+  //     const {data} = res;
+  //     const {rates} = data;
+  //     this.ratesList = rates.map((rate) => {
+  //       return {
+  //         ccy: rate.ccy,
+  //         sale: rate.sale
+  //       }
+  //     })
+  //     console.log(data)
+  //   })
+  // }
 }
